c33b5ea7f131a15ec29ec42d0216563833c01db1
Joshua Prpich <joshuaprpich@gmail.com>
2017-12-12 21:00:06 -0500

    changes made by joshua

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,43 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
+
+  def create
+    @question = Question.create(question_params)
+
+    redirect_to questions_path
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update(question_params)
+
+    redirect_to questions_path
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+
+    redirect_to questions_path  
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body).merge(user_id: current_user.id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'questions/index'
-
-  get 'questions/new'
-
+  
+  root to: 'questions#index'
   devise_for :users
+
+  resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'questions#index'
+  
 end
